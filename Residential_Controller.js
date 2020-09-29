5bd9bb4c2d0aed0581415da3665cfc7ef2636ae2
RaphVaudreuil <raphvaudreuil-basket@hotmail.com>
2020-09-28 20:17:08 -0400

    JS

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -1,11 +1,32 @@
 class Column {
-    defineColumn(status, id, minimumFloor, maximumFloor, elevatorList, buttonsUpList, buttonsDownList) {
-        this.status = status
-        this.id = id
-        this.minimumFloor = minimumFloor
-        this.maximumFloor = maximumFloor
-        this.elevatorList = []
-        this.buttonsUpList = []
-        this.buttonsDownList = []
+
+    constructor(status, id, minimumFloor, maximumFloor, numberOfElevators, numberOfFloors, elevatorList, buttonsUpList, buttonsDownList) {  
+        this.status = status;
+        this.id = id;
+        this.minimumFloor = minimumFloor;
+        this.maximumFloor = maximumFloor;
+        this.numberOfElevators = numberOfElevators;
+        this.numberOfFloors = numberOfFloors;
+        this.elevatorList = [];
+        this.buttonsUpList = [];
+        this.buttonsDownList = [];
+    
+    
+        function createElevatorList(numberOfElevators) {
+            for(elevator = 0; elevator < numberOfElevators; elevator++) {
+               var elevator = new Elevator(elevatorID, "Idle", 1, "Closed", numberOfFloors);
+               this.elevatorList.push(elevator);
+            }
+        }
+        function createButtonsUpList(numberOfFloors) {
+            for(floor = 0; floor < numberOfFloors; floor++)
+        }       
     }
 }
+
+
+
+
+
+
+
